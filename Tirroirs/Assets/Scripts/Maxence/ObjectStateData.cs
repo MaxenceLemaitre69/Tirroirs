034eb242d55e52358e8dd2e4ce79a5b32bfd6164
Alba <chrismartinez030506@gmail.com>
2026-01-09 15:28:51 +0100

    Mécanique fonctionnel

--- Tirroirs/Assets/Scripts/Maxence/ObjectStateData.cs
+++ Tirroirs/Assets/Scripts/Maxence/ObjectStateData.cs
@@ -1,3 +1,15 @@
+//using UnityEngine;
+//
+//[CreateAssetMenu(fileName = "ObjectStateData", menuName = "Game/Object State Data")]
+//public class ObjectStateData : ScriptableObject
+//{
+//    public GameObject initialPrefab;
+//    public GameObject afterEventPrefab;
+//
+//    [Header("Notes du carnet")]
+//    [TextArea(5, 12)] public string noteState1;  // état initial
+//    [TextArea(5, 12)] public string noteState2;  // après event
+//}
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ObjectStateData", menuName = "Game/Object State Data")]
