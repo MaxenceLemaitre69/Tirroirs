ce65a426bd3a7c46e8f7ebc8fb9eaef9a68af4ea
Maxence Lemaitre <maxencelemaitre028@gmail.com>
2026-03-06 10:42:28 +0100

    fin

---
+++ Tirroirs/Assets/Scripts/Maxence/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Bouton Jouer
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SceneTestChris"); 
+        // Remplace "GameScene" par le nom de ta scène de jeu
+    }
+
+    // Bouton Quitter
+    public void QuitGame()
+    {
+        Debug.Log("Quitter le jeu");
+        Application.Quit();
+    }
+}
